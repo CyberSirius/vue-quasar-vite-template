7ec6ef62264a8d2e070d62a9f834e701b9ef159e
Strashimir Rashev <strashimir.rashev@nevexis.com>
2021-10-13 08:58:12 +0300

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,27 @@
+<template>
+  <h1>{{ msg }}</h1>
+
+
+  <p></p>
+  <q-btn type="button" @click="count++">count is: {{ count }}</q-btn>
+  <p>
+    Edit
+    <code>components/HelloWorld.vue</code> to test hot module replacement.
+  </p>
+</template>
+<script lang="ts" setup>
+import {ref} from 'vue'
+
+defineProps<{
+  msg: string
+}>()
+
+
+const count = ref(0)
+</script>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/main.ts
@@ -0,0 +1,8 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import 'material-icons/iconfont/material-icons.css';
+import "quasar/dist/quasar.prod.css";
+import {Quasar} from "quasar";
+
+
+createApp(App).use(Quasar).mount('#app')
